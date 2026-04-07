62594f7568f8eae8ee49c0b4d98813a21d0e56c7
dayu <dayu@dayu.jp>
2026-04-07 20:19:00 +0900

    feat(sky.go): 太陽高度を取得できるようにした

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/Code-Hex/synchro v0.5.4
 	github.com/ugjka/go-tz/v2 v2.2.8
 )
+
+require github.com/dayu255/sun-level v0.0.0-20260407090227-ef17ffbf9749 // indirect
--- sky.go
+++ sky.go
@@ -7,49 +7,46 @@ import (
 	"time"
 	"slices"
 
+	"github.com/dayu255/sun-level"
 	"github.com/ugjka/go-tz/v2"
 	"github.com/Code-Hex/synchro/iso8601"
 )
 
-type queryHandler struct{}
+type color struct{
+	color1 string
+	color2 string
+	color3 string
+	color4 string
+}
 
-func (qh queryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+type context struct {
+	scene string
+	weather string
+	cloudNess float32
+	sunLevel int
+}
+
+type data struct {
+	color color
+	css_gradient string
+	context context
+}
+
+
+func decimalToHex(n int) string {
+	if n == 0 {
+		return "0"
 	}
 
-	queryParams := r.URL.Query()
+	chars := "0123456789ABCDEF"
+
+	var ans string
+	for n > 0 {
+		ans = string(chars[n % 16]) + ans
+		n /= 16
+	}
 	
-	t, tErr := iso8601.ParseDateTime(queryParams.Get("time"))
-
-	lon, err := strconv.ParseFloat(queryParams.Get("lon"), 64)
-	if err != nil && tErr != nil {
-		http.Error(w, "Invalid longitude", http.StatusBadRequest)
-		return
-	}
-
-	lat, err := strconv.ParseFloat(queryParams.Get("lat"), 64)
-	if err != nil && tErr != nil {
-		http.Error(w, "Invalid latitude", http.StatusBadRequest)
-		return
-	}
-
-	if tErr != nil {
-		t, tErr = calTime(lon, lat)
-		if tErr != nil {
-			http.Error(w, "Invalid query", http.StatusBadRequest)
-		}
-	}
-
-	weather := queryParams.Get("weather")
-	weatherTypes := []string{"clear", "sunny", "cloudy", "rain", "snow", "fog", "thunder"}
-	if result := slices.Contains(weatherTypes, weather); result == false {
-		weather = "unknown"
-	}
-
-	w.Write([]byte(t.Format(time.RFC3339)))
-	w.Write([]byte(weather))
+	return ans
 }
 
 func calTime(longitude float64, latitude float64) (time.Time, error) {
@@ -67,6 +64,44 @@ func calTime(longitude float64, latitude float64) (time.Time, error) {
 	return time.Now().In(location), nil
 }
 
+func NewSky(t time.Time, weather string, sunLevel float64) color {
+			
+}
+
+type queryHandler struct{}
+
+func (qh queryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	queryParams := r.URL.Query()
+	
+	t, tErr := iso8601.ParseDateTime(queryParams.Get("time"))
+	if tErr != nil {
+		t = time.Now()
+	}
+
+	lon, lonErr := strconv.ParseFloat(queryParams.Get("lon"), 64)
+	lat, latErr := strconv.ParseFloat(queryParams.Get("lat"), 64)
+
+	weather := queryParams.Get("weather")
+	weatherTypes := []string{"clear", "sunny", "cloudy", "rain", "snow", "fog", "thunder"}
+	if result := slices.Contains(weatherTypes, weather); result == false {
+		weather = "unknown"
+	}
+
+	if tErr != nil && lonErr != nil && latErr != nil {
+		w.Write([]byte("Query Error. We need latitude and longitude or time."))
+		return
+	}
+
+	sky := NewSky(t, weather, sun.CalSunLevel(t, lat, lon))
+	
+	w.Write()
+}
+
 func main() {
 	s := http.Server{
 		Addr:         ":8080",
@@ -77,6 +112,7 @@ func main() {
 	}
 	log.Println("Listening Port", s.Addr)
 	err := s.ListenAndServe()
+	
 	if err != nil {
 		if err != http.ErrServerClosed {
 			panic(err)
